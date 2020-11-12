365e2ca22caf940273b6ce17166b7aec68c25fad
Shuhei Kadowaki <aviatesk@gmail.com>
2020-11-12 23:36:24 +0900

    debug

--- test/debug.jl
+++ test/debug.jl
@@ -19,5 +19,35 @@ macro benchmark(ex, kwargs...)
     end end
 end
 
+@eval Base begin
+    function list_deletefirst!(q::InvasiveLinkedList{T}, val::T) where T
+        val.queue === q || return
+        head = q.head::T
+        if head === val
+            if q.tail::T === val
+                q.head = q.tail = nothing
+            else
+                q.head = val.next::T
+            end
+        else
+            head_next = head.next
+            while head_next !== val
+                head = head_next
+                head_next = head.next::Union{T, Nothing}
+            end
+            head = head::T # assume `val` is found
+            if q.tail::T === val
+                head.next = nothing
+                q.tail = head
+            else
+                head.next = val.next::T
+            end
+        end
+        val.next = nothing
+        val.queue = nothing
+        return q
+    end
+end
+
 interp, frame = @benchmark rand(Bool)
 print_reports(stdout, interp.reports; annotate_types = true)
